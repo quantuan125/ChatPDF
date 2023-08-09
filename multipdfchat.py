5d3ccf18a7a3645e3fa48f0b3296c2c1a070098b
quantuan125 <quantuan125hl@gmail.com>
2023-08-09 10:51:39 +0200

    Changes to the display of uploaded files

--- multipdfchat.py
+++ multipdfchat.py
@@ -30,9 +30,9 @@ from InstructorEmbedding import INSTRUCTOR
 def display_pdfs(file_path):
             with open(file_path, "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="700" type="application/pdf"></embed>'
+            pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="600" height="700"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
-    
+            
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
