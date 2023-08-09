c336f3eebf211e59b5a2b6bf3cdf4b079b7523fb
quantuan125 <quantuan125hl@gmail.com>
2023-08-09 18:46:13 +0200

    what

--- multipdfchat.py
+++ multipdfchat.py
@@ -30,7 +30,7 @@ from InstructorEmbedding import INSTRUCTOR
 def display_pdfs(file_path):
             with open(file_path, "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = F'<iframe credentialless src="data:application/pdf;base64,{base64_pdf}" width="600" height="700"></iframe>'
+            pdf_display = F'<iframe credentialless src="data:application/pdf;base64,{base64_pdf}?igu=1" width="600" height="700"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
             
 def get_pdf_text(pdf_docs):
